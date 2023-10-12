e6a93a6fa455b955922d75b10db4ebd3c6c0be51
Vishnu-3101 <gadigevishnu.vardhan2021@vitstudent.ac.in>
2023-10-12 22:42:16 +0530

    Added simpler solution to buyAndSellStocks

--- Array/BestTimetoBuyandSellStocksII/BTBSSII.cpp
+++ Array/BestTimetoBuyandSellStocksII/BTBSSII.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
+
+class Solution1 {
 public:
     int maxProfit(vector<int>& prices) {
         int maxpro=0;
@@ -14,6 +15,22 @@ public:
             }
             if(sell<maxpro){
                 buy = prices[i];
+            }
+        }
+        return maxpro;
+    }
+};
+
+// a much simpler solution for the problem. If we get a profit the next day sell the stock.
+// Add the profit to total profit
+
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxpro=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]>prices[i-1]){
+                maxpro+=(prices[i]-prices[i-1]);
             }
         }
         return maxpro;
