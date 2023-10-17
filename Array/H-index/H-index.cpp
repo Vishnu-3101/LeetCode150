db66cb3daab64a1840edd3644e9e4044aa21fbf1
Vishnu-3101 <gadigevishnu.vardhan2021@vitstudent.ac.in>
2023-10-17 09:49:41 +0530

    Added problems on 17/10/2023

---
+++ Array/H-index/H-index.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end(),greater<int>());
+        int hIndex=0;
+        for(int i=0;i<citations.size();i++){
+            if(citations[i]>=i+1){
+                hIndex=i+1;
+            }
+            else{
+                break;
+            }
+        }
+        return hIndex;
+    }
+};
