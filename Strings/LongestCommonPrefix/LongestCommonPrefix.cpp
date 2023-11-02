4a31aabdace2c06fe37a17414f0a4221e07ccee4
Vishnu-3101 <gadigevishnu.vardhan2021@vitstudent.ac.in>
2023-11-02 10:22:22 +0530

    Added codes on 11/1/2023

--- Strings/LongestCommonPrefix/LongestCommonPrefix.cpp
+++ Strings/LongestCommonPrefix/LongestCommonPrefix.cpp
@@ -1,9 +1,86 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// We can do this using trie data structure
+// This problem can be done in 3 ways.
+// 1. Comparing first string with all the other strings to get the common prefix
+// 2. Using trie data structure
+// 3. Sorting the array of strings and comparing the char in 1st and last string to get the prefix
 
-class Solution {
+// We can do this using trie data structure
+struct Node{
+    Node* node[26];
+    int nodeCount=0;
+    bool end = false;
+    bool isPresent(char c){
+        return (node[c-'a']!=NULL);
+    }
+    void putNode(char c,Node* newnode){
+        node[c-'a']=newnode;
+    }
+    Node* setNode(char c){
+        return node[c-'a'];
+    }
+
+    void setEnd(){
+        end=true;
+    }
+
+    void increCount(){
+        nodeCount++;
+    }
+};
+
+class Trie{
+    public:
+    Node* root;
+    Trie(){
+        root = new Node();
+    }   
+    void insert(string word){
+        Node* node = root;
+        for(int i=0;i<word.length();i++){
+            if(!node->isPresent(word[i])){
+                node->putNode(word[i],new Node());
+                node->increCount();
+            }
+            node = node->setNode(word[i]);
+        }
+        node->setEnd();
+    }
+    int check(string s){
+        Node* node = root;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(node->nodeCount>1){
+                return count;
+            }
+            if(node->nodeCount==1 && node->end==true){
+                return count;
+            }
+            node = node->setNode(s[i]);
+            count++;
+        }
+        return count;
+    }
+};
+
+class Solution1:public Trie{
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        if(n==1){
+            return strs[0];
+        }
+        for(int i=0;i<n;i++){
+            insert(strs[i]);
+        }
+        int index = check(strs[n-1]);
+        return strs[n-1].substr(0,index);
+    }
+};
+
+// This is better than brute force approch with time complexity O(N2)
+class Solution2{
 public:
     string longestCommonPrefix(vector<string>& strs) {
         string prefix = strs[0];
