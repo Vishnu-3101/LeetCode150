2c3990e0d447bc95f2a9b289e20dd19071be57df
Vishnu-3101 <gadigevishnu.vardhan2021@vitstudent.ac.in>
2024-05-30 08:31:25 +0530

    Added one more solution

--- Math/PalindromeNumber/Palindrome.cpp
+++ Math/PalindromeNumber/Palindrome.cpp
@@ -1,6 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Reversing only half of the number
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return (x == reversed) || (x == reversed / 10);
+    }
+};
+
+// Reversing complete number
+
 class Solution {
 public:
     bool isPalindrome(int x) {
