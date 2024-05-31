a8c04dd35524a8a19e125c07f9ee37f86d9ba6e5
Vishnu-3101 <gadigevishnu.vardhan2021@vitstudent.ac.in>
2024-05-31 07:57:38 +0530

    Added codes to singleNumberII

--- BitManipulation/SingleNumberII/SingleNumberII.cpp
+++ BitManipulation/SingleNumberII/SingleNumberII.cpp
@@ -15,3 +15,35 @@ public:
         return nums[nums.size()-1];
     }
 };
+
+// using bitwise
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<32;i++){
+            int sum=0;
+            for(auto num:nums){
+                sum+=(num>>i)&1;
+            }
+            sum%=3;
+            ans = ans|(sum<<i);
+        }
+        return ans;
+    }
+};
+
+// Using buckets
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones=0,twos=0;
+        for(auto i:nums){
+            ones = (ones^i)&(~twos);
+            twos = (twos^i)&(~ones);
+        }
+        return ones;
+    }
+};
+
