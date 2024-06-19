a4dd567668382c9b7f2941fb8bd9635c36fb3336
Vishnu-3101 <gadigevishnu.vardhan2021@vitstudent.ac.in>
2024-06-19 09:15:45 +0530

    Added a code in backtracking

--- BackTracking/Combinations/Combinations.cpp
+++ BackTracking/Combinations/Combinations.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Optimal approach
+
 class Solution {
 public:
     vector<vector<int>> result;
@@ -15,6 +17,29 @@ public:
             combination.pop_back();
             
         }
+    } 
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> combination;
+        FindCombinations(n,k,1,combination);
+        return result;
+    }
+};
+
+// Using take or not take 
+
+class Solution {
+public:
+    vector<vector<int>> result;
+    void FindCombinations(int n,int k, int i, vector<int> &combination){
+        if(k==0){
+            result.push_back(combination);
+            return;
+        }
+        if(i>n) return; 
+        combination.push_back(i);
+        FindCombinations(n,k-1,i+1,combination);
+        combination.pop_back();
+        FindCombinations(n,k,i+1,combination);            
     } 
     vector<vector<int>> combine(int n, int k) {
         vector<int> combination;
